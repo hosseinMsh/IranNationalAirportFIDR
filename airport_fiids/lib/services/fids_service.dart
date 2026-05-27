fa63a6d707fdc5d89c0b3210847cf494fa9d6b81
mac <mac@apple.com>
2026-05-27 15:15:49 +0330

    Add Tabriz, Isfahan, and Ahvaz airports to the list

--- airport_fiids/lib/services/fids_service.dart
+++ airport_fiids/lib/services/fids_service.dart
@@ -12,6 +12,9 @@ class FidsService {
     Airport(id: '2', name: 'مهرآباد', nameEn: 'Mehrabad', latitude: 35.6892, longitude: 51.3134, urlPath: '/2/%D8%A7%D8%B7%D9%84%D8%A7%D8%B9%D8%A7%D8%AA-%D9%BE%D8%B1%D9%88%D8%A7%D8%B2-%D9%81%D8%B1%D9%88%D8%AF%DA%AF%D8%A7%D9%87-%D9%85%D9%87%D8%B1%D8%A2%D8%A8%D8%A7%D8%AF'),
     Airport(id: '102', name: 'مشهد', nameEn: 'Mashhad', latitude: 36.2352, longitude: 59.6439, urlPath: '/102/%D8%A7%D8%B7%D9%84%D8%A7%D8%B9%D8%A7%D8%AA-%D9%BE%D8%B1%D9%88%D8%A7%D8%B2-%D9%81%D8%B1%D9%88%D8%AF%DA%AF%D8%A7%D9%87-%D9%85%D8%B4%D9%87%D8%AF'),
     Airport(id: '1', name: 'شیراز', nameEn: 'Shiraz', latitude: 29.5392, longitude: 52.5898, urlPath: '/1/%D8%A7%D8%B7%D9%84%D8%A7%D8%B9%D8%A7%D8%AA-%D9%BE%D8%B1%D9%88%D8%A7%D8%B2-%D9%81%D8%B1%D9%88%D8%AF%DA%AF%D8%A7%D9%87-%D8%B4%D9%8A%D8%B1%D8%A7%D8%B2'),
+    Airport(id: '103', name: 'تبریز', nameEn: 'Tabriz', latitude: 38.1339, longitude: 46.2350, urlPath: '/103/%D8%A7%D8%B7%D9%84%D8%A7%D8%B9%D8%A7%D8%AA-%D9%BE%D8%B1%D9%88%D8%A7%D8%B2-%D9%81%D8%B1%D9%88%D8%AF%DA%AF%D8%A7%D9%87-%D8%AA%D8%A8%D8%B1%D9%8A%D8%B2'),
+    Airport(id: '114', name: 'اصفهان', nameEn: 'Isfahan', latitude: 32.7508, longitude: 51.8613, urlPath: '/114/%D8%A7%D8%B7%D9%84%D8%A7%D8%B9%D8%A7%D8%AA-%D9%BE%D8%B1%D9%88%D8%A7%D8%B2-%D9%81%D8%B1%D9%88%D8%AF%DA%AF%D8%A7%D9%87-%D8%A7%D8%B5%D9%81%D9%87%D8%A7%D9%86'),
+    Airport(id: '401', name: 'اهواز', nameEn: 'Ahvaz', latitude: 31.3374, longitude: 48.7626, urlPath: '/401/%D8%A7%D8%B7%D9%84%D8%A7%D8%B9%D8%A7%D8%AA-%D9%BE%D8%B1%D9%88%D8%A7%D8%B2-%D9%81%D8%B1%D9%88%D8%AF%DA%AF%D8%A7%D9%87-%D8%A7%D9%87%D9%88%D8%A7%D8%B2'),
   ];
 
   Future<Map<String, List<Flight>>> fetchFlights(Airport airport) async {
