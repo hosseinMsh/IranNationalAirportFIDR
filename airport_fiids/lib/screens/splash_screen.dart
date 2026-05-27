563a4cc77c649a5eaf8d492f278208031ba4a158
mac <mac@apple.com>
2026-05-27 15:18:18 +0330

    Add fade-in animation with auto-navigation to home

--- airport_fiids/lib/screens/splash_screen.dart
+++ airport_fiids/lib/screens/splash_screen.dart
@@ -8,12 +8,46 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeIn;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1200),
+    );
+    _fadeIn = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+    _controller.forward();
+
+    Future.delayed(const Duration(seconds: 2), () {
+      if (mounted) _navigateToHome();
+    });
+  }
+
+  void _navigateToHome() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (_) => const AirportListScreen()),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Splash Screen'),
+      body: FadeTransition(
+        opacity: _fadeIn,
+        child: Center(
+          child: Text('Splash Screen'),
+        ),
       ),
     );
   }
