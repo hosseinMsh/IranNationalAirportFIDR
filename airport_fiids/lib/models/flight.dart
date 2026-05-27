6215e7d8346c5bd160cd857df4bdf301779e1a68
mac <mac@apple.com>
2026-05-27 15:14:25 +0330

    Add Flight data model with status and schedule fields

---
+++ airport_fiids/lib/models/flight.dart
@@ -0,0 +1,23 @@
+class Flight {
+  final String dayTime;
+  final String airline;
+  final String flightNumber;
+  final String origin;
+  final String status;
+  final String actualTime;
+  final String register;
+  final String aircraft;
+  final String date;
+
+  Flight({
+    required this.dayTime,
+    required this.airline,
+    required this.flightNumber,
+    required this.origin,
+    required this.status,
+    required this.actualTime,
+    required this.register,
+    required this.aircraft,
+    required this.date,
+  });
+}
